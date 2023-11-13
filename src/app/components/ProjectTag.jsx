ad292143e338286d2fc67aa5580a2fd9872349a7
JorgeMartinez <flaco100602@gmail.com>
2023-11-13 00:08:05 -0500

    Portafolio-web1

--- src/app/components/ProjectTag.jsx
+++ src/app/components/ProjectTag.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ProjectTag = ({ name, onClick, isSelected }) => {
   const buttonStyles = isSelected
-    ? "text-white border-primary-500"
+    ? "text-white border-green-400"
     : "text-[#ADB7BE] border-slate-600 hover:border-white";
   return (
     <button
